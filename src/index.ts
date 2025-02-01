97cf66ce0dee40845ddb167d1045418ff8d9cbee
Zaki Maulana Hidayat <zaki.bsj.a23@gmail.com>
2025-02-01 10:28:09 +0700

    add API regis & validation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 import router from "./routes/api";
 
 const app = express();
-
 const PORT = 3000;
+
+app.use(bodyParser.json());
+// Notes From DeepSeek AI
+// Mulai dari versi Express 4.16.0, bodyParser sudah diintegrasikan ke dalam Express.js, 
+// sehingga Anda tidak perlu menginstal bodyParser secara terpisah. 
+// Anda bisa langsung menggunakan express.json() dan express.urlencoded()
 
 app.use("/api", router);
 
