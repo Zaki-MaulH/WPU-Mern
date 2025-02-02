dcb609471cf5080d95d0f47255d3f46aef8599e1
Zaki Maulana Hidayat <zaki.bsj.a23@gmail.com>
2025-02-02 17:53:21 +0700

    encrypt password before saving to DB

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,4 +5,8 @@ dotenv.config();
 export const DATABASE_URL: string = process.env.DATABASE_URL || (() => {
     throw new Error("DATABASE_URL is not defined in .env file");
   })();
+
+export const SECRET: string = process.env.SECRET || (() => {
+    throw new Error("SECRET is not defined in .env file");
+  })();
   
