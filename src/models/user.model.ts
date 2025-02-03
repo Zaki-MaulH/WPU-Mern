bddc6147b22a621af2ef051885cc1a09720ea15e
Zaki Maulana Hidayat <zaki.bsj.a23@gmail.com>
2025-02-03 10:52:06 +0700

    API auth login

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -59,6 +59,14 @@ userSchema.pre("save", function (next) {
     user.password = encrypt(user.password);
     next();
 })
+
+// Middleware to delete password before sending response
+userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+    delete user.password;
+    return user;
+}
+
 const UserModel = mongoose.model("User", userSchema);
 
 export default UserModel;
